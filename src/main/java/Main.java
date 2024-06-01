a6d126a8c3fd828126f5c47488844656a40effd3
ar1024 <ar1024@yandex.ru>
2024-06-01 08:37:12 +0300

    AR1024 CALCULATOR (fix 0 pay)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,14 +16,16 @@ public class Main {
         String pravilno= calculator.roubleEnd(PoSkolkoPlatit);
         //идем на извращения, дабы поменять точку на запятую.
         // функция replace не работает и т.п. тоже не работают
-        System.out.println("итог: "+PoSkolkoPlatit);
-        DecimalFormat myFloatFormatter = new DecimalFormat("#.##");
-        String dolgStr=myFloatFormatter.format(PoSkolkoPlatit);
-        int zap=dolgStr.indexOf(",");
-        String part1=dolgStr.substring(0,zap);
-        String part2=dolgStr.substring(zap+1,dolgStr.length());
+        if (PoSkolkoPlatit>0) {
+            //System.out.println("итог: " + PoSkolkoPlatit);
+            DecimalFormat myFloatFormatter = new DecimalFormat("#.##");
+            String dolgStr = myFloatFormatter.format(PoSkolkoPlatit);
+            int zap = dolgStr.indexOf(",");
+            String part1 = dolgStr.substring(0, zap);
+            String part2 = dolgStr.substring(zap + 1, dolgStr.length());
 
-        // Выводим итог
-        System.out.println("Каждый должен заплатить по: "+part1+"."+part2+" "+pravilno);
+            // Выводим итог
+            System.out.println("Каждый должен заплатить по: " + part1 + "." + part2 + " " + pravilno);
+        }
     }
 }
